e062c45528f601cbf100f6e4f54a036512dcd00b
Guillermo San Martín <guille.seba0802@gmail.com>
2023-05-09 18:38:42 -0300

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-function Header ({ handleChange, handleSort, handleSubmit, sort, search, err }) {
+function Header({ handleChange, handleSort, handleSubmit, sort, search, err }) {
   return (
     <header className='header'>
       <h1>Movie searcher</h1>
-      <form className='form' onSubmit={handleSubmit}>
-        <input name='query' value={search} onChange={handleChange} placeholder='Spiderman, Interestellar, Origin...' />
+      <form
+        className='form'
+        onSubmit={handleSubmit}
+      >
+        <input
+          name='query'
+          value={search}
+          onChange={handleChange}
+          placeholder='Spiderman, Interestellar, Origin...'
+        />
         <button>Search</button>
-        <label className='sort-label' name='sort'>Sort Alphabeticaly
-          <input className='sort-input' name='sort' type='checkbox' onChange={handleSort} checked={sort} />
+        <label
+          className='sort-label'
+          htmlFor='sort'
+          name='sort'
+        >
+          Sort Alphabeticaly
+          <input
+            className='sort-input'
+            name='sort'
+            id='sort'
+            type='checkbox'
+            onChange={handleSort}
+            checked={sort}
+          />
         </label>
       </form>
       {err && <p style={{ color: 'red' }}>{err}</p>}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
